82de4bfa420bd2e4b726b6a2c1a56f775ae1f90a
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-29 02:43:06 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,9 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+    :montague =>
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
       #your key/value pairs here!
       }, 
